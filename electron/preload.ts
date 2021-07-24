41dc1d2b8ca5176c41080805eaf4e257afea6450
Soikat Rahman <hello.dev.sr@gmail.com>
2021-07-24 19:39:27 +0600

    final temp

--- electron/preload.ts
+++ electron/preload.ts
@@ -2,18 +2,6 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { ipcRenderer, contextBridge } from 'electron';
 
-// declare global {
-// 	namespace NodeJS {
-// 		interface Global {
-// 			ipcRenderer: IpcRenderer;
-// 		}
-// 	}
-// }
-// Since we disabled nodeIntegration we can reintroduce
-// needed node functionality here
-// process.once('loaded', () => {
-// 	global.ipcRenderer = ipcRenderer;
-// });
 declare global {
 	// eslint-disable-next-line
 	interface Window {
